3894587e1693a20a53c03c905dd0755103ced2cd
gowrisoft <prabhakargudi@gowrisoftsolutions.com>
2024-12-17 14:46:17 +0530

    Delete springboot-erp-hr directory

--- springboot-erp-hr/src/main/java/com/gentech/hr/repository/RelievingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gentech.hr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gentech.hr.entity.RelievingReport;
-
-public interface RelievingRepository extends JpaRepository<RelievingReport, Long>{
-
-}
