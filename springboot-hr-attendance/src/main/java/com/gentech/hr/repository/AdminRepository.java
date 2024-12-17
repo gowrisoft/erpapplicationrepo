eb1057f2cf290748697c5084e550c266988e9156
AmitVikram80 <amit@gowrisoftsolutions.com>
2024-12-17 14:06:19 +0530

    Delete springboot-hr-attendance directory

--- springboot-hr-attendance/src/main/java/com/gentech/hr/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gentech.hr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gentech.hr.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long>{
-
-}
