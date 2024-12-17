e29717b6b666b16792b2ef44d8258275176c85f3
AmitVikram80 <amit@gowrisoftsolutions.com>
2024-12-17 14:05:56 +0530

    Delete springboot-erp-hr directory

--- springboot-erp-hr/src/main/java/com/gentech/hr/repository/JoiningRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gentech.hr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gentech.hr.entity.JoiningReport;
-
-public interface JoiningRepository extends JpaRepository<JoiningReport, Long>{
-
-}
--- springboot-erp-hr/src/main/java/com/gentech/hr/repository/PrintRelievingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gentech.hr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gentech.hr.entity.PrintRelievingReports;
-
-public interface PrintRelievingRepository extends JpaRepository<PrintRelievingReports,Long> {
-
-}
