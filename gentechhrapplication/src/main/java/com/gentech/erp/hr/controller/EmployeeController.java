f70acc4d8998d09f614e097da57cacd28a218b22
AmitVikram80 <amit@gowrisoftsolutions.com>
2024-12-25 23:21:22 +0530

    Updated

--- gentechhrapplication/src/main/java/com/gentech/erp/hr/controller/EmployeeController.java
+++ gentechhrapplication/src/main/java/com/gentech/erp/hr/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.gentech.erp.hr.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,17 +19,17 @@ public class EmployeeController {
     private EmployeeService employeeService;
     @PostMapping("/addEmployee")
     ResponseEntity<EmployeeDto> addEmployee(@RequestBody EmployeeDto employeeDto){
-        return new ResponseEntity<EmployeeDto>(employeeService.addEmployee(employeeDto), HttpStatusCode.valueOf(200));
+        return new ResponseEntity<>(employeeService.addEmployee(employeeDto), HttpStatus.OK);
     }
 
     @GetMapping("/allEmployees")
     ResponseEntity<List<EmployeeDto>> getAllEmployees(){
-        return new ResponseEntity<List<EmployeeDto>>(employeeService.getAllEmployees(),HttpStatusCode.valueOf(200));
+        return new ResponseEntity<>(employeeService.getAllEmployees(),HttpStatus.OK);
     }
 
     @GetMapping("/getEmployeeById")
     ResponseEntity<EmployeeDto> getEmployeeById(@RequestParam long id){
-        return new ResponseEntity<EmployeeDto>(employeeService.getEmployeeById(id),HttpStatusCode.valueOf(200));
+        return new ResponseEntity<>(employeeService.getEmployeeById(id),HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
@@ -38,6 +39,6 @@ public class EmployeeController {
 
     @DeleteMapping("/deleteEmployeeById")
     ResponseEntity<String> deleteEmployeeById(@RequestParam long id){
-        return new ResponseEntity<String>(employeeService.deleteEmployeeById(id),HttpStatusCode.valueOf(200));
+        return new ResponseEntity<>(employeeService.deleteEmployeeById(id),HttpStatus.OK);
     }
 }
