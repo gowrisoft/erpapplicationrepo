26f31c0cb13870388d755a02b8aaac584b553132
AmitVikram80 <amit@gowrisoftsolutions.com>
2025-01-08 08:18:34 +0530

    gentechhrapplication directory

--- gentechhrapplication/src/main/java/com/gentech/erp/hr/repository/PrintJoiningReportsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gentech.erp.hr.repository;
-
-import com.gentech.erp.hr.entity.PrintJoiningReports;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrintJoiningReportsRepository extends JpaRepository<PrintJoiningReports, Long> {
-
-}
